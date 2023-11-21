265092bf2468602bd56715c5572be15f92669110
hassan <hassanmarzouk042@gmail.com>
2023-11-21 11:48:33 -0800

    edit initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:selivery_driver/app/app.dart';
+import 'package:selivery_driver/core/helper/notifictions_helper.dart';
 import 'core/services/cache_storage_services.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CacheStorageServices.init();
-  Firebase.initializeApp();
+  FirebaseMessagingService;
+ await Firebase.initializeApp();
   runApp(const SeliveryDriver());
 }
